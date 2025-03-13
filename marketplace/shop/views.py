82d562aeff84164581b4754556d6101dcb7eca16
Kai120789 <zoomer120789@gmail.com>
2025-03-13 23:37:32 +0300

    feat: added filters for catalog page

--- marketplace/shop/views.py
+++ marketplace/shop/views.py
@@ -2,14 +2,38 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 from shop.models import Category, Product, Brand, ProductVariant
 
+from django.shortcuts import render
+from .models import Product, Category, Brand
+from django.db.models import Q
+
 def product_list(request):
-    products_list = Product.objects.all()
+    category_id = request.GET.get("category")
+    brand_id = request.GET.get("brand")
+    sort_by = request.GET.get("sort", "default_price")  # Сортировка по умолчанию — по цене
 
-    paginator = Paginator(products_list, 12)
-    page_number = request.GET.get('page')
-    products = paginator.get_page(page_number)
+    products = Product.objects.all()
 
-    return render(request, 'shop/product_list.html', {'products': products})
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    if brand_id:
+        products = products.filter(brand_id=brand_id)
+
+    if sort_by in ["default_price", "-default_price", "created_at", "-created_at"]:
+        products = products.order_by(sort_by)
+
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
+
+    return render(request, "shop/product_list.html", {
+        "products": products,
+        "categories": categories,
+        "brands": brands,
+        "selected_category": category_id,
+        "selected_brand": brand_id,
+        "sort_by": sort_by,
+    })
+
 
 def brand_list(request):
     brands = Brand.objects.all()
